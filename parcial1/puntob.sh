0ec5135b7f60f9a9842d1d1f0cc71d33a058ad73
Fran139 <francocasais17@gmail.com>
2024-10-19 17:45:15 -0300

    puntob.sh

--- parcial1/puntob.sh
+++ parcial1/puntob.sh
@@ -1,11 +1,11 @@
 lsblk
-sudo fdisk /dev/sdp
-sudo mkfs.ext4 /dev/sdp1
-sudo mkfs.ext4 /dev/sdp2
-sudo mkfs.ext4 /dev/sdp3
-sudo mkfs.ext4 /dev/sdp4
-sudo mount /dev/sda1 /mnt/p1
-sudo mount /dev/sda2 /mnt/p2
-sudo mount /dev/sda3 /mnt/p3
-sudo mount /dev/sda4 /mnt/p4
+sudo fdisk /dev/sdd
+sudo mkfs.ext4 /dev/sdd1
+sudo mkfs.ext4 /dev/sdd2
+sudo mkfs.ext4 /dev/sdd3
+sudo mkfs.ext4 /dev/sdd4
+sudo mount /dev/sdd1 /mnt/p1
+sudo mount /dev/sdd2 /mnt/p2
+sudo mount /dev/sdd3 /mnt/p3
+sudo mount /dev/sdd4 /mnt/p4
 lsblk
